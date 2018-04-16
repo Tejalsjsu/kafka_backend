fef1b92a2b8023f45fdb400c49bf779787417f8d
tejalpadharia <36251006+tejalpadharia@users.noreply.github.com>
2018-04-15 19:10:53 -0700

    Final commit
    Final commit

--- services/fetch.js
+++ services/fetch.js
@@ -17,19 +17,56 @@ exports.handle_request = (data, callback)  => {
                 console.log("Connection established");
             }
             let dbo = db.db("freelancer");
-            dbo.collection("projects").find(
-                    { 'employerId':ObjectId(data.userId)}
-                )
-                .toArray(function(err,result){
-                if(err){
-                    console.log("Error in getting result");
-                    res.status = 401;
-                }else if(result.length) {
-                    console.log("fetched successfully from db " + result);
-                    res.status = 201;
-                    res.data = result;
-                }
-            });
+            switch (data.project){
+                case 'ALL':
+                    //Fetch all projects
+                    dbo.collection("projects").find({'status': data.status})
+                        .toArray(function(err,result){
+                            if(err){
+                                console.log("Error in getting result");
+                                res.status = 401;
+                            }else if(result.length) {
+                                console.log("fetched successfully from db " + result);
+                                res.status = 201;
+                                res.data = result;
+                            }
+                        });
+                    break;
+
+                case 'CURRENTUSER':
+                    //Fetch projects only for logged in user as an employer
+                    dbo.collection("projects").find(
+                        { 'employerId':ObjectId(data.userId), 'status': data.status}
+                    )
+                        .toArray(function(err,result){
+                            if(err){
+                                console.log("Error in getting result");
+                                res.status = 401;
+                            }else if(result.length) {
+                                console.log("fetched successfully from db " + result);
+                                res.status = 201;
+                                res.data = result;
+                            }
+                        });
+                    break;
+
+                case 'FREELANCER':
+                    //Fetch projects only for logged in user on which he is currently working i.e as a freelancer
+                    dbo.collection("projects").find(
+                        { 'Hired':ObjectId(data.userId), 'status': data.status}
+                    )
+                        .toArray(function(err,result){
+                            if(err){
+                                console.log("Error in getting result");
+                                res.status = 401;
+                            }else if(result.length) {
+                                console.log("fetched successfully from db " + result);
+                                res.status = 201;
+                                res.data = result;
+                            }
+                        });
+                    break;
+            }
         });
 
         callback(null, res);
